4ff00a41f0dff9dc6b0aa82cc5a285edc67b6078
Washington-Kimani <kimaniwashington39@gmail.com>
2025-07-06 07:17:54 +0300

    Some changes for render hosting

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,13 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 /*MIDDLEWARES*/
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
 
 /*ALL ROUTES*/
 app.use("/register", register);
