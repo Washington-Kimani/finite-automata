39835af145e943e6570be57f6c5472e5f773023a
Washington-Kimani <codefam.dev@gmail.com>
2026-01-05 14:34:58 +0300

    edited the route for the index.html

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,11 @@ dotenv.config();
 /*MIDDLEWARES*/
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "frontend", "dist", "index.html")
+  );
+});
 
 
 /*ALL ROUTES*/
