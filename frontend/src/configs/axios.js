307feedac04ab2b198fbcc16524e1ef76ca383ea
Washington-Kimani <codefam.dev@gmail.com>
2026-02-16 12:26:41 +0300

    fix: changed the api url

--- frontend/src/configs/axios.js
+++ frontend/src/configs/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = `https://finite-automata.onrender.com/`
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default axios;
